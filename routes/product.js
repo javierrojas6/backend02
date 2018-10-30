3b9d18b41d2125202d4cfe01f3e5668f418ffa1e
Javier Rojas <javierrojas6@gmail.com>
2018-10-29 19:10:38 -0500

    crud de pruductos completo

--- routes/product.js
+++ routes/product.js
@@ -2,6 +2,16 @@ var express = require('express');
 var router = express.Router();
 var object = require('../modules/objectsAndTypes');
 
+router.get('/:id', (req, res, next) => {
+  object.get('Product', req.params.id, 1)
+    .then(response => {
+      res.json({ status: true, content: response });
+    })
+    .catch(response => {
+      res.json({ status: false, content: response });
+    });
+});
+
 router.post('/save', (req, res, next) => {
   object.save([
     'nombre',
@@ -22,6 +32,16 @@ router.put('/save/:id', (req, res, next) => {
     'nombre',
     'description'
   ], values, 'Product')
+    .then(response => {
+      res.json({ status: true, content: response });
+    })
+    .catch(response => {
+      res.json({ status: false, content: response });
+    });
+});
+
+router.delete('/delete/:id', (req, res, next) => {
+  object.delete('Product', req.params.id)
     .then(response => {
       res.json({ status: true, content: response });
     })
