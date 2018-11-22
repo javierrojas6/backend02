5f3bea97d25714931bb8cdd7da24ed2746858dad
Javier Rojas <javierrojas6@gmail.com>
2018-11-21 19:01:35 -0500

    pruebas funcionando

--- spec/user-register.spec.js
+++ spec/user-register.spec.js
@@ -20,12 +20,31 @@ it('Registro de Usuario', function () {
       content: "user registrado"
     })
     .expect('jsonTypes', 'content[1]', {
+      command: 'model',
+      type: 'user',
+    })
+    .expect('jsonTypes', 'content[1].content', {
+      id: Joi.number().required(),
       email: user.email,
       password: '*****',
       firstName: user.firstName,
       lastName: user.lastName
     })
     .then(response => {
-      console.log(response);
+      let data = response._json;
+      let { status, content } = data;
+      let tmpUser = content[1].content;
+
+      return frisby
+        .post('http://localhost:3000/user/login', { email: user.email, password: user.password })
+        .expect('status', 200)
+        .expect('json', 'status', true)
+        .expect('jsonTypes', 'content', {
+          id: Joi.number().required(),
+          email: tmpUser.email,
+          token: Joi.string().required()
+        })
+        .expect('json', 'content.email', user.email)
+        .expect('json', 'content.password', '*****')
     })
 });
